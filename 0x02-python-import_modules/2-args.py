b567209d896f39151c5b19390b6c8031df8fb846
Bereket Teferi <bereketteferi87@gmail.com>
2024-01-18 12:30:03 +0300

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 import sys
 
-if __name__ == "__main__":  # Ensures code only runs when executed directly, not imported
-   num_args = len(sys.argv) - 1  # Exclude the script name
+if __name__ == "__main__":
+    num_args = len(sys.argv) - 1
 
-   if num_args == 0:
-       print("0 arguments.")
-   else:
-       print(f"{num_args} {'argument' if num_args == 1 else 'arguments'}:")
-       for i in range(1, num_args + 1):
-           print(f"{i}: {sys.argv[i]}")
-
+    if num_args == 0:
+        print("0 arguments.")
+    else:
+        print(f"{num_args} {'argument' if num_args == 1 else 'arguments'}:")
+        for i in range(1, num_args + 1):
+            print(f"{i}: {sys.argv[i]}")
