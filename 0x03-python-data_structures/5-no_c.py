0917960f48fa5fe4b1058aa9f502f913ffefff7b
Bereket Teferi <bereketteferi87@gmail.com>
2024-01-20 11:18:38 +0300

    Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
 
 def no_c(my_string):
-    if my_string:
-        new_string = ""
-        for i in my_string:
-            if i.lower() != 'c':
-                new_string += i
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
     return new_string
