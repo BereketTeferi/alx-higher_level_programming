662c90dd7026f8a3c9e15b3fee9e3e39262b0348
Bereket Teferi <bereketteferi87@gmail.com>
2024-01-20 11:27:40 +0300

    Task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-   element1 = tuple_a[0] if len(tuple_a) >= 1 else 0
-   element2 = tuple_a[1] if len(tuple_a) >= 2 else 0
-   element3 = tuple_b[0] if len(tuple_b) >= 1 else 0
-   element4 = tuple_b[1] if len(tuple_b) >= 2 else 0
-   return (element1 + element3, element2 + element4)
+    element1 = tuple_a[0] if len(tuple_a) >= 1 else 0
+    element2 = tuple_a[1] if len(tuple_a) >= 2 else 0
+    element3 = tuple_b[0] if len(tuple_b) >= 1 else 0
+    element4 = tuple_b[1] if len(tuple_b) >= 2 else 0
+    return (element1 + element3, element2 + element4)
