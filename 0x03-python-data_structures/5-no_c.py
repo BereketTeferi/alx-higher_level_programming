8820f6cd7a8f747ae29a0c830bfc763bf6079b24
Bereket Teferi <bereketteferi87@gmail.com>
2024-01-20 11:17:33 +0300

    Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 def no_c(my_string):
-    new_string = ""
-    for i in my_string:
-        if i.lower() != 'c':
-            new_string += i
+    if my_string:
+        new_string = ""
+        for i in my_string:
+            if i.lower() != 'c':
+                new_string += i
     return new_string
